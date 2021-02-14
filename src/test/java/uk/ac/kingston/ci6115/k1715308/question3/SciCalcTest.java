b1b1686f73a3e590a29256a22ecec4b73344892d
Gustavo Zapata <k1715308@kingston.ac.uk>
2021-02-14 20:20:35 +0000

    add suite test as a runner to test all the calculations from the SciCalcTest.java file

--- src/test/java/uk/ac/kingston/ci6115/k1715308/question3/SciCalcTest.java
+++ src/test/java/uk/ac/kingston/ci6115/k1715308/question3/SciCalcTest.java
@@ -6,46 +6,26 @@
 package uk.ac.kingston.ci6115.k1715308.question3;
 
 //JUnit 5
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 //import org.junit.jupiter.api.Test;
 //import static org.junit.jupiter.api.Assertions.*;
 
 //JUnit 4.11
-import org.junit.Test;
-import static org.junit.Assert.*;
+//import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  *
  * @author k1715308
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    SciCalcIntTest.class,
+    SciCalcDoubleTest.class,
+    SciCalcArrayTest.class,
+    SciCalcListTest.class
+})
 public class SciCalcTest {
-    
-    public SciCalcTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+    // the class remains empty
+    // used only as a holder for the above annotations
 }
